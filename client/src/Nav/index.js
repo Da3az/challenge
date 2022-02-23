c9f65acbe404459df12725e39673c4ed5192da41
chakib <c-a-b@live.fr>
2022-02-23 17:50:22 +0100

    changed the nav title

--- client/src/Nav/index.js
+++ client/src/Nav/index.js
@@ -4,7 +4,7 @@ import LogoSvg from '../logo.svg'
 const Nav = () => (
     <div className="p-6 flex flex items-center justify-center">
         <img className="p-4" src={LogoSvg} alt="logo" width={250}/>
-        <h1 className="font-bold">Technical Assignment</h1>
+        <h1 className="font-bold">Technical Challenge</h1>
     </div>
 )
 
